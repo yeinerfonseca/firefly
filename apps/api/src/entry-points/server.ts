7a19761d1ba0de827578df414bfa5416222235b1
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-10-28 15:06:41 -0400

    feat: apply middlewares after app has started

--- apps/api/src/entry-points/server.ts
+++ apps/api/src/entry-points/server.ts
@@ -1,13 +1,27 @@
 import { Server } from 'node:http'
 import { AddressInfo } from 'node:net'
 import express, { Express } from 'express'
+import helmet from 'helmet'
 import { router } from 'express-file-routing'
+import { logger } from '@api/libraries/logger'
+import { errorHandler, errorMiddleware } from '@api/libraries/error-handling'
+import { addRequestId } from '@api/libraries/middlewares'
 
 let connection: Server
 
 async function startWebServer(): Promise<AddressInfo> {
+  // logger.configureLogger(
+  //   {
+  //     prettyPrint: Boolean(
+  //       configurationProvider.getValue('logger.prettyPrint'),
+  //     ),
+  //   },
+  //   true,
+  // )
   const app: Express = express()
   app.use('/', await router())
+  defineCommonMiddlewares(app)
+  defineErrorHandlingMiddleware(app)
   const apiAddress = await openConnection(app)
   return apiAddress
 }
@@ -26,9 +40,22 @@ async function openConnection(app: express.Application): Promise<AddressInfo> {
   return new Promise((resolve) => {
     const port = process.env.PORT || 0
     connection = app.listen(port, () => {
+      errorHandler.listenToErrorEvents(connection)
       resolve(connection.address() as AddressInfo)
     })
   })
+}
+
+function defineErrorHandlingMiddleware(expressApp: express.Application) {
+  expressApp.use()
+}
+
+function defineCommonMiddlewares(app: express.Application) {
+  app.use(addRequestId)
+  app.use(helmet())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(errorMiddleware)
 }
 
 export { startWebServer, stopWebServer }
--- apps/api/start.ts
+++ apps/api/start.ts
@@ -1,9 +1,10 @@
-// import { startWebServer } from '@api/entry-points/server'
+import { startWebServer } from '@api/entry-points/server'
+import { logger } from '@api/libraries/logger'
 
-// startWebServer()
-//   .then((response) => {
-//     // logger
-//   })
-//   .catch((error) => {
-//     // errorhandler
-//   })
+startWebServer()
+  .then((response) => {
+    logger.info(`The app has started succesfully ${response}`)
+  })
+  .catch((error) => {
+    // errorhandler
+  })
