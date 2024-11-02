ffd2f3a732456df8557ca899d16aeada9011e142
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-11-02 14:42:52 -0400

    fix(web): install custom font and set it up in tailwind

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,58 +3,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      heading: ['Massilia', 'helvetica', 'sans-serif'],
-      body: ['Mundial', 'helvetica', 'sans-serif'],
-    },
-    fontWeight: {
-      ultra: 800,
-      bold: 700,
-      demibold: 700,
-      regular: 400,
-      light: 300,
-    },
-    colors: {
-      blur: 'rgba(#6F4DF3, .85)',
-      overlay: {
-        plume: 'rgba(#26122F, .7)',
-        'russian-violet': 'rgba(#2C0F48, .55)',
-        black: 'rgba(#20202A, .45)',
-        white: 'rgba(#FFFFFF, .3)',
-        purple: 'rgba(#6F4DF3, .15)',
+    extend: {
+      fontFamily: {
+        display: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Open Sans',
+          'Helvetica Neue',
+          'sans-serif',
+        ],
       },
-      bg: {
-        l1: '#190B28',
-        l2: '#DBFF72',
-        l3: '#6F4DF3',
-        l4: '#B9AAFA',
-        l5: '#BCE6EC',
-        'dark-purple': '#553DAD',
-        yellow: {
-          deep: '#1F220F',
-          light: '#6D7558',
-        },
+      fontSize: {
+        lg: ['7.2rem', '7.4rem'],
       },
-      divider: {
-        white: 'rgba(255, 255, 255, .45)',
-        lilac: '#B9AAFA',
-        plumec: 'rgba(#26122F, .25)',
+      colors: {
+        main: '#151515',
       },
-      semantic: {
-        green: '#7DDB8B',
-        orange: '#FFB267',
-        violet: '#B688FF',
-        red: '#FF9998',
-        ruby: '#FF99FF',
-        'sky-blue': '#99CDFF',
-        'slate-blue': '#9999FF',
-        'yellow-sun': '#FEE36D',
-        'green-blue': '#C9E7DF',
-      },
-    },
-    screens: {
-      md: '768px',
-      lg: '1440px',
     },
   },
   plugins: [],
