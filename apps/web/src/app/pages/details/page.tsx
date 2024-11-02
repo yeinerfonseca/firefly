e3cbb4dc6b5952e43a69aad8b37a530bd4e38807
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-11-02 14:43:45 -0400

    feat(web): create initial layout for details page

--- apps/web/src/app/pages/details/page.tsx
+++ apps/web/src/app/pages/details/page.tsx
@@ -6,17 +6,25 @@ export default function DetailsPage() {
     'https://media.rawg.io/media/resize/1280/-/games/09b/09b41c1a2c5761c5b1772a4ae238bb0e.jpg'
 
   return (
-    <section className="absolute inset-0 -z-10 h-full w-full">
-      <div
-        className={styles.artwork}
-        style={
-          {
-            '--artwork-url': `url("${artworkUrl}")`,
-          } as React.CSSProperties
-        }
-      ></div>
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full">
+        <div
+          className={styles.artwork}
+          style={
+            {
+              '--artwork-url': `url("${artworkUrl}")`,
+            } as React.CSSProperties
+          }
+        ></div>
+      </div>
 
-      <div className="max-w-[960px]"></div>
-    </section>
+      <section className="max-w-[960px] m-auto flex gap-[4.8rem] text-white">
+        <div className="flex-1">
+          <h1 className="font-bold text-lg">Silent Hill 2</h1>
+          <div></div>
+        </div>
+        <div className="flex-1">Gallery</div>
+      </section>
+    </>
   )
 }
